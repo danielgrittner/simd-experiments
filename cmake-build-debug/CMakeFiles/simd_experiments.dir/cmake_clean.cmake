79ecc2a23abd7fde0593c4e446fea7833f0e2f5a
Daniel Grittner <danielgrittner@web.de>
2020-05-01 22:33:42 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/simd_experiments.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simd_experiments.dir/main.cpp.o"
+  "simd_experiments"
+  "simd_experiments.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simd_experiments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
